5179470dfa0331874cbf3a308ef0cf162ec23b49
yang99 <yang.zhang99@outlook.com>
2023-12-26 13:40:06 +0800

    chapter15

--- src/main/java/com/yang/Test.java
+++ src/main/java/com/yang/Test.java
@@ -9,25 +9,42 @@ import static java.util.stream.Collectors.toList;
 public class Test {
 
     public static void main(String[] args) {
-//        Float f =  Float.valueOf(12.33f);
+
+        try {
+            System.out.println(1 / 0);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.out.println("---");
+            System.out.println(e.getMessage());
+            System.out.println("---");
+            System.out.println(e.getCause());
+
+            System.out.println(e.getCause().getCause());
+        }
+
 //
-//        System.out.println(f);
+////        Float f =  Float.valueOf(12.33f);
+////
+////        System.out.println(f);
+////
+////        BigDecimal b = new BigDecimal(String.valueOf(f));
+////
+////        System.out.println(b);
 //
-//        BigDecimal b = new BigDecimal(String.valueOf(f));
+////        Goods goods1 = new Goods().setId(1).setName("apple");
+//        Goods goods2 = new Goods().setId(null).setName("banana");
+//        List<Goods> goods = new ArrayList<>();
+////        goods.add(goods1);
+//        goods.add(goods2);
 //
-//        System.out.println(b);
-
-//        Goods goods1 = new Goods().setId(1).setName("apple");
-        Goods goods2 = new Goods().setId(null).setName("banana");
-        List<Goods> goods = new ArrayList<>();
-//        goods.add(goods1);
-        goods.add(goods2);
-
-
-        List<Integer> goodsIds = goods.stream().filter(Objects::nonNull).map(Goods::getId).distinct().collect(toList());
-
-        System.out.println(goodsIds);
-
-        System.out.println(goodsIds.isEmpty());
+//
+//        List<Integer> goodsIds = goods.stream().filter(Objects::nonNull).map(Goods::getId).distinct().collect(toList());
+//
+//        System.out.println(goodsIds);
+//
+//        System.out.println(goodsIds.isEmpty());
     }
 }
+
+
+
